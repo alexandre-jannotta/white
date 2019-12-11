46c93b6d10658fbec8acd63959a4852ac98c2357
Alexandre JANNOTTA <jannotta.alexandre@gmail.com>
2019-12-11 16:21:18 +0100

    Call car service

--- white-fitnesse/src/main/java/com/cgi/paris/fs/comex/white/fitnesse/fixture/TellWhatTheCarMustDoWhen.java
+++ white-fitnesse/src/main/java/com/cgi/paris/fs/comex/white/fitnesse/fixture/TellWhatTheCarMustDoWhen.java
@@ -1,13 +1,26 @@
 package com.cgi.paris.fs.comex.white.fitnesse.fixture;
 
+import com.cgi.paris.fs.comex.white.core.bean.Car;
+import com.cgi.paris.fs.comex.white.core.service.CarService;
 import fit.ColumnFixture;
 
 public class TellWhatTheCarMustDoWhen extends ColumnFixture {
 
+    private final CarService carService = new CarService();
+
     private String light;
 
+    private Car car;
+
+    @Override
+    protected void executeIfNeeded() throws Exception {
+        this.car = new Car();
+        this.carService.handleLight(car, this.light);
+        super.executeIfNeeded();
+    }
+
     public String action() {
-        return null;
+        return this.car.getAction();
     }
 
 }
