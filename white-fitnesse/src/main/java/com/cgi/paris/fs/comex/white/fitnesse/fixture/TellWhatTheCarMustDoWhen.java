14a4437f49dc5d4b1edef288bb2df970da9d04ac
Alexandre JANNOTTA <jannotta.alexandre@gmail.com>
2019-12-11 16:17:08 +0100

    Add Fixtures

---
+++ white-fitnesse/src/main/java/com/cgi/paris/fs/comex/white/fitnesse/fixture/TellWhatTheCarMustDoWhen.java
@@ -0,0 +1,13 @@
+package com.cgi.paris.fs.comex.white.fitnesse.fixture;
+
+import fit.ColumnFixture;
+
+public class TellWhatTheCarMustDoWhen extends ColumnFixture {
+
+    private String light;
+
+    public String action() {
+        return null;
+    }
+
+}
